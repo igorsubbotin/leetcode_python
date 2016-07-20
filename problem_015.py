f0f9d9c40b42734d0bb97e7929f15c8db4f62224
Igor <moat1981@gmail.com>
2016-07-20 23:50:19 +0300

    Solve Problem 015 - 3Sum

--- problem_015.py
+++ problem_015.py
@@ -6,33 +6,25 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        p = []
-        for i in xrange(len(nums)):
-            for j in xrange(i + 1, len(nums)):
-                p.append(([nums[i] + nums[j], i, j, nums[i], nums[j]]))
-        d = {}
-        for i in xrange(len(nums)):
-            n = nums[i]
-            if n not in d:
-                d[n] = set()
-            d[n].add(i)
         r = set()
         res = []
-        for s, i, j, a, b in p:
-            c = 0 - s
-            if c in d:
-                ix = d[c]
-                ln = len(ix)
-                if i in ix:
-                    ln -= 1
-                if j in ix:
-                    ln -= 1
-                if ln > 0:
-                    l = [a, b, c]
+        for i in xrange(len(nums)):
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s == 0:
+                    l = [nums[i], nums[j], nums[k]]
                     l.sort()
                     t = tuple(l)
                     if t not in r:
                         r.add(t)
                         res.append(l)
+                    j += 1
+                    continue
+                if s < 0:
+                    j += 1
+                else:
+                    k -= 1
         return res
         
