74a8172fed00b7d8986bf2be4e547f67412e9da5
Igor <moat1981@gmail.com>
2016-07-27 01:50:25 +0300

    Solve Problem 031 - Next Permutation

--- problem_031.py
+++ problem_031.py
@@ -5,45 +5,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
-        inv = findFirstInversion(nums)
-        if inv is None:
-            quicksort(nums)
-            return
-        print inv
-        i, j = inv
-        t = nums[j]
-        nums[j] = nums[i]
-        nums[i] = t
-        quicksort(nums, i + 1)
-        print nums
+        ix = -1
+        for i in reversed(xrange(len(nums) - 1)):
+            if nums[i] < nums[i + 1]:
+                ix = i
+                break
+        if ix != -1:
+            for i in xrange(ix + 1, len(nums)):
+                if nums[i] <= nums[ix]:
+                    i -= 1
+                    break
+            swap(nums, i, ix)
+        for i in xrange((len(nums) - 1 - ix) / 2):
+            swap(nums, ix + i + 1, len(nums) - i - 1)
         
-        
-def findFirstInversion(a):
-    mx = -1
-    mx_ix = None
-    for i in reversed(xrange(len(a))):
-        item = a[i]
-        for j in reversed(xrange(i)):
-            if a[j] < item and j > mx:
-                mx = j
-                mx_ix = [j, i]
-    return mx_ix
-    
-def partition(array, begin, end):
-    pivot = begin
-    for i in xrange(begin+1, end+1):
-        if array[i] <= array[begin]:
-            pivot += 1
-            array[i], array[pivot] = array[pivot], array[i]
-    array[pivot], array[begin] = array[begin], array[pivot]
-    return pivot
-    
-    
-def quicksort(array, begin=0, end=None):
-    if end is None:
-        end = len(array) - 1
-    if begin >= end:
-        return
-    pivot = partition(array, begin, end)
-    quicksort(array, begin, pivot-1)
-    quicksort(array, pivot+1, end)
+def swap(a, i, j):
+    t = a[i]
+    a[i] = a[j]
+    a[j] = t
