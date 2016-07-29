4ddf6388d7548871ed18d50c546a1bc3686f6aaf
Igor <moat1981@gmail.com>
2016-07-29 12:42:07 +0300

    Solve Problem 039 - Combination Sum

--- problem_039.py
+++ problem_039.py
@@ -8,32 +8,38 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        candidates.sort()
         s = []
         r = set()
         res = []
         c = []
-        for x in candidates:
+        ix = 0
+        for i in xrange(len(candidates)):
+            x = candidates[i]
             if x == target:
                 res.append([x])
                 continue
             if x > target:
-                continue
+                break
             c.append(x)
-            s.append(([x], x))
+            s.append(([x], x, ix))
+            ix += 1
         candidates = c
         while len(s) > 0:
-            a, n = s.pop()
-            for x in candidates:
+            a, n, i = s.pop()
+            for j in xrange(i, len(candidates)):
+                x = candidates[j]
                 nn = n + x
                 if nn > target:
-                    continue
+                    break
                 c = copy(a)
                 c.append(x)
                 if nn == target:
+                    c.sort()
                     t = tuple(c)
                     if t not in r:
                         r.add(t)
                         res.append(t)
                     continue
-                s.append((c, nn))
+                s.append((c, nn, j))
         return res
