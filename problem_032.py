0342d86a967a9036b27fcd0ba6ad23342e89e62c
Igor <moat1981@gmail.com>
2016-08-02 02:29:20 +0300

    Solve Problem 032 - Longest Valid Parentheses

--- problem_032.py
+++ problem_032.py
@@ -6,15 +6,15 @@ class Solution(object):
         :rtype: int
         """
         a = [0] * (len(s) + 1)
-        open = []
+        stk = []
         mx = 0
         for i in xrange(len(s)):
             x = s[i]
             if x == "(":
-                open.append(a[i])
+                stk.append(a[i])
             else:
-                if len(open) > 0:
-                    v = open.pop() + a[i] + 2
+                if len(stk) > 0:
+                    v = stk.pop() + a[i] + 2
                     a[i+1] = v
                     mx = max(mx, v) 
         return mx
