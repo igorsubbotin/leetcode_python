b8540e7461fa6131920b36ff8333329d90bd3cf8
Igor <moat1981@gmail.com>
2016-12-05 22:34:29 +0300

    Solve Problem 136 - Single Number

---
+++ problem_136.py
@@ -0,0 +1,11 @@
+# Single Number - https://leetcode.com/problems/single-number/
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for x in nums:
+            res ^= x
+        return res
